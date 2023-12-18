51043223d56228dbec9483fb92a111fb3cb9b0b6
ElthonD <elthon.rivas@gmail.com>
2023-12-18 10:30:18 -0600

    j

--- app1.py
+++ app1.py
@@ -16,35 +16,33 @@ import requests
 import warnings
 warnings.filterwarnings('ignore')
 
-def createPage():
+
     
-    @st.cache_data(show_spinner='Cargando Datos... Espere...', persist=True)
-    def load_HR():
+@st.cache_data(show_spinner='Cargando Datos... Espere...', persist=True)
+def load_HR():
 
-        rutaA = r'./data/Historico de Robos MELI.xlsx'
-        Robos = pd.read_excel(rutaA, sheet_name = "Data")
-        Robos = Robos.drop(['Operadores','CM', 'Línea Reacción'], axis=1)
-        Robos['Fecha'] = Robos['Fecha'].dt.strftime('%m/%d/%Y')
-        Robos['Fecha'] = pd.to_datetime(Robos['Fecha'], format="%Y/%m/%d", infer_datetime_format=True)
-        Robos['Año'] = Robos.Fecha.apply(lambda x: x.year)
-        Robos['MesN'] = Robos['Fecha'].apply(lambda x: x.month)
-        Robos['Mes'] = Robos['MesN'].map({1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"})
-        Robos['Dia'] = Robos.Fecha.apply(lambda x: x.day)
-        Robos['Fecha y Hora'] = pd.to_datetime(Robos['Fecha y Hora'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-        Robos = Robos.dropna()
+    rutaA = r'./data/Historico de Robos MELI.xlsx'
+    Robos = pd.read_excel(rutaA, sheet_name = "Data")
+    Robos = Robos.drop(['Operadores','CM', 'Línea Reacción'], axis=1)
+    Robos['Fecha'] = Robos['Fecha'].dt.strftime('%m/%d/%Y')
+    Robos['Fecha'] = pd.to_datetime(Robos['Fecha'], format="%Y/%m/%d", infer_datetime_format=True)
+    Robos['Año'] = Robos.Fecha.apply(lambda x: x.year)
+    Robos['MesN'] = Robos['Fecha'].apply(lambda x: x.month)
+    Robos['Mes'] = Robos['MesN'].map({1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"})
+    Robos['Dia'] = Robos.Fecha.apply(lambda x: x.day)
+    Robos['Fecha y Hora'] = pd.to_datetime(Robos['Fecha y Hora'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+    Robos = Robos.dropna()
 
-        return Robos
+    return Robos
 
 
-    df = load_HR()
+df = load_HR()
 
-    df = df[['Año', 'Tipo evento', 'Hora', 'Estado', 'Tramo']]
+df = df[['Año', 'Tipo evento', 'Hora', 'Estado', 'Tramo']]
 
-    st.dataframe(df)
+st.dataframe(df)
 
-    return True
-
-    """
+"""
     c1, c2, c3, c4, c5, c6, c7 = st.columns(7)
     with c1:
         st.header("1")
