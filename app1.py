9679c6bbbb7de46d8e2bd815ab689f68007b334a
ElthonD <elthon.rivas@gmail.com>
2023-12-18 11:33:24 -0600

    update

--- app1.py
+++ app1.py
@@ -42,7 +42,7 @@ df = df[['Año', 'Tipo evento', 'Fecha y Hora', 'Estado', 'Tramo', 'Mes', 'Día'
 
 df['Estatus'] = df['Tipo evento'].map(lambda x: 1 if x == "Consumado" else 0)
 
-st.dataframe(df.style.applymap(lambda x: 'color: red' if x == 0 else 'color: green'))
+st.dataframe(df.style.applymap(lambda x: 'color: red' if any('Consumado' in words for words in x.split()) else 'color: green'))
 
 
 #st.dataframe(df['Estatus'].style.map(lambda x: 'color: red' if x == 0 else 'color: green'))
